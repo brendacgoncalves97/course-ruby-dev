6098b1b20a15126116008ce39bf85f036a0657da
brendacgoncalves97 <brenda.goncalves965@gmail.com>
2023-02-19 21:38:35 -0300

    add files

---
+++ desafio1.rb
@@ -0,0 +1,18 @@
+#Pergunta o nome e o guarda
+print "Informe seu nome: "
+nome = gets.chomp
+
+#Pergunta o sobrenome e o guarda
+
+print "Agora informe seu sobrenome: "
+sobrenome = gets.chomp
+
+# Por ultimo pergunta sua idade e a guarda
+
+print "E por último informe sua idade: "
+
+idade = gets.chomp
+
+# Printa tudo em tela
+
+puts "Olá #{nome} #{sobrenome}, sua idade é #{idade} anos."
